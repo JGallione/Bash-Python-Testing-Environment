683ab96d3031a658a7628d855b8da2835aeba03e
Joseph Gallione <josephgallione@gmail.com>
2020-03-17 13:55:07 -0400

    update NLP_Service_Demo.py

--- NLP_Service_Demo.py
+++ NLP_Service_Demo.py
@@ -4,4 +4,14 @@ import tensorflow
 import nltk
 
 print ("hello")
-print ("greets")
+
+Service_Ticket = input ("Enter Ticket:")
+
+VPN_Arr = ["vpn", "VPN"]
+
+if any(c in Service_Ticket for c in VPN_Arr):
+    print ("contains vpn")
+else:
+    print ("does not contain vpn")
+
+raw_input = input ('Press Enter to exit')
